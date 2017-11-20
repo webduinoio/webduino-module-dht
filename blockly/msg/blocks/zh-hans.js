75975fc771a982bda9d93eb6afb58a477612e951
osk2 <sc60714@gmail.com>
2017-11-20 16:33:07 +0800

    Update string

--- blockly/msg/blocks/zh-hans.js
+++ blockly/msg/blocks/zh-hans.js
@@ -1,6 +1,6 @@
 Blockly.Msg.WEBDUINO_DHT_NEW = "温湿度传感器，脚位";
 Blockly.Msg.WEBDUINO_DHT_GET = "侦测温湿度，每";
-Blockly.Msg.WEBDUINO_DHT_GET_TIME = "毫秒 ( 1/1000 秒 ) 撷取一次";
+Blockly.Msg.WEBDUINO_DHT_GET_TIME = "毫秒 ( 1/1000 秒 ) 侦测一次";
 Blockly.Msg.WEBDUINO_DHT_GET_DO = "执行";
 Blockly.Msg.WEBDUINO_DHT_GET_NOW = "所测得目前的";
 Blockly.Msg.WEBDUINO_DHT_GET_T = "温度 (摄氏)";
